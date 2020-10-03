4b190963d77a6ea4979d88e4c542ed5011a6b224
CDRMV <46399086+CDRMV@users.noreply.github.com>
2020-10-03 20:10:55 +0200

    Add Transport Animation +  a few fixes

--- Units/UEL0002/UEL0002_script.lua
+++ Units/UEL0002/UEL0002_script.lua
@@ -244,8 +244,6 @@ UEL0002 = Class(ACUUnit) {
 		self:HideBone('Back_Upgrade_B03', true)
         self:HideBone('Back_Upgrade_B04', true)
         self:SetupBuildBones()
-        self.HasLeftPod = false
-        self.HasRightPod = false
         # Restrict what enhancements will enable later
         self:AddBuildRestriction( categories.UEF * (categories.BUILTBYTIER2COMMANDER + categories.BUILTBYTIER3COMMANDER) )
     end,
